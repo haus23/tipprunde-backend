cb41e6aeda1aac4f0016020f8e5a32a4b8655c29
Micha Buchholz <micha@haus23.net>
2023-03-11 23:01:52 +0100

    refactor: caching firebase data

--- src/routes/api/v2/index.ts
+++ src/routes/api/v2/index.ts
@@ -2,13 +2,14 @@ import { Router } from 'express';
 
 import { Championship, Player } from '@haus23/tipprunde-types';
 import { collection } from 'lib/firebase';
+import { cachedFunction } from 'lib/unstorage/cached-function';
 
 export const apiV2 = Router()
   .get('/championships', async (req, res) => {
-    const championships = await collection<Championship>('championships');
+    const championships = await cachedFunction('championships', () => collection<Championship>('championships'));
     res.json(championships.filter((c) => c.published).sort((a, b) => b.nr - a.nr));
   })
   .get('/players', async (req, res) => {
-    const players = await collection<Player>('/players');
+    const players = await cachedFunction('players', () => collection<Player>('/players'));
     res.json(players);
   });
